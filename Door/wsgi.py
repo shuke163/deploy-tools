e37bd0b747f849b1e3453ed9a9a41ebfeaf53312
shuke <shuke_163@163.com>
2020-04-13 12:44:49 +0800

    [update] 去掉注释

--- Door/wsgi.py
+++ Door/wsgi.py
@@ -1,11 +1,3 @@
-'''
-@Author: your name
-@Date: 2020-04-13 12:02:08
-@LastEditTime: 2020-04-13 12:19:21
-@LastEditors: Please set LastEditors
-@Description: In User Settings Edit
-@FilePath: /deploy-backend/door/wsgi.py
-'''
 """
 WSGI config for door backend project.
 
